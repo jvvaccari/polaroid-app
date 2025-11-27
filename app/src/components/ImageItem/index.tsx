bbe02f712236d8f9fc34d42093eafbd2c4fa6964
jvvaccari <jv_vaccari@live.com>
2025-11-27 13:27:43 -0300

    refactor: reorganizar estrutura do componente ImageItem para melhor legibilidade

--- app/src/components/ImageItem/index.tsx
+++ app/src/components/ImageItem/index.tsx
@@ -8,20 +8,26 @@ interface ImageItemProps {
 
 const ImageItem = ({ src, alt, sx }: ImageItemProps) => {
   return (
-    <Box
-      component="img"
-      src={src}
-      alt={alt}
-      draggable={false}
-      onDragStart={(e) => e.preventDefault()}
-      sx={{
-        width: "100%",
-        height: "auto",
-        objectFit: "cover",
-        display: "block",
-        ...sx,
-      }}
-    />
+    <Box>
+      <Box
+        component="img"
+        src={src}
+        alt={alt}
+        draggable={false}
+        onDragStart={(e) => e.preventDefault()}
+        sx={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          display: "block",
+          ...sx,
+        }}
+      />
+      {/* Debug: mostra o valor do src na tela */}
+      <Box sx={{ color: "white", fontSize: 12, mt: 1 }}>
+        src: {src || "undefined"}
+      </Box>
+    </Box>
   );
 };
 
