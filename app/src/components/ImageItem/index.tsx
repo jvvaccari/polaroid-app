8124fd5ceb7184cb66938d7b73f6d6b9d0ee4ed9
jvvaccari <jv_vaccari@live.com>
2025-11-27 13:50:09 -0300

    refactor: remover caixa de texto desnecessária do componente ImageItem

--- app/src/components/ImageItem/index.tsx
+++ app/src/components/ImageItem/index.tsx
@@ -8,34 +8,20 @@ interface ImageItemProps {
 
 const ImageItem = ({ src, alt, sx }: ImageItemProps) => {
   return (
-    <Box>
-      <Box
-        sx={{
-          bgcolor: "black",
-          padding: 10,
-          color: "white",
-          fontSize: 12,
-          mt: 1,
-          zIndex: 1000,
-        }}
-      >
-        {src || "undefined"}
-      </Box>
-      <Box
-        component="img"
-        src={src}
-        alt={alt}
-        draggable={false}
-        onDragStart={(e) => e.preventDefault()}
-        sx={{
-          width: "100%",
-          height: "auto",
-          objectFit: "cover",
-          display: "block",
-          ...sx,
-        }}
-      />
-    </Box>
+    <Box
+      component="img"
+      src={src}
+      alt={alt}
+      draggable={false}
+      onDragStart={(e) => e.preventDefault()}
+      sx={{
+        width: "100%",
+        height: "auto",
+        objectFit: "cover",
+        display: "block",
+        ...sx,
+      }}
+    />
   );
 };
 
