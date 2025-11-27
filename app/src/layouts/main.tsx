de69ddcae52fde049fd522a20a0d2f57d20cce01
jvvaccari <jv_vaccari@live.com>
2025-11-27 14:39:23 -0300

    refactor: ajustar a formatação do código no componente MainLayout para melhor legibilidade

--- app/src/layouts/main.tsx
+++ app/src/layouts/main.tsx
@@ -1,29 +1,32 @@
 import { Container, Stack, type SxProps, type Theme } from "@mui/material";
 
 interface MainLayoutProps {
-    children: React.ReactNode;
-    sx?: SxProps<Theme>;
+  children: React.ReactNode;
+  sx?: SxProps<Theme>;
 }
 
 const MainLayout = ({ children, sx }: MainLayoutProps) => {
-    return (
-        <Stack
-            sx={{
-                width: "100vw",
-                height: "100vh",
-                justifyContent: "center",
-                alignItems: "center",
-                overflow: "hidden",
-                margin: 0,
-                padding: 0,
-                ...sx,
-            }}
-        >
-            <Container maxWidth="xl" sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                {children}
-            </Container>
-        </Stack>
-    )
+  return (
+    <Stack
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
+        margin: 0,
+        padding: 0,
+        ...sx,
+      }}
+    >
+      <Container
+        maxWidth="xl"
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        {children}
+      </Container>
+    </Stack>
+  );
 };
 
 export default MainLayout;
